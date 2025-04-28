ebd00069a3bc26b6aa206831079c3778859becda
bemnet773 <bemnettesfu7@gmail.com>
2025-04-28 21:17:14 +0300

    first commit

--- remove.py
+++
@@ -1,3 +0,0 @@
-my_list = [1, 2, 3, 2]
-my_list.remove(2)  # Removes the first occurrence of 2
-# my_list now becomes [1, 3, 2]
--- startswith.py
+++
@@ -1,2 +0,0 @@
-text = "Hello World"
-print(text.startswith("Hello"))  # Output: True
