e09b230242a83fc6587764ed19f64a707827e76d
bemnet773 <bemnettesfu7@gmail.com>
2025-05-01 22:05:19 +0300

    second commit

---
+++ copy.py
@@ -0,0 +1,3 @@
+my_dict = {'a': 1, 'b': 2}
+new_dict = my_dict.copy()
+# new_dict is {'a': 1, 'b': 2}
---
+++ fromkeys.py
@@ -0,0 +1,3 @@
+keys = ['a', 'b', 'c']
+new_dict = dict.fromkeys(keys, 0)
+# new_dict is {'a': 0, 'b': 0, 'c': 0}
