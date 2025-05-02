f6f1b383781c0e118b5bfed2384110422f9f0eeb
bemnet773 <bemnettesfu7@gmail.com>
2025-05-02 21:52:58 +0300

    third commit

---
+++ popitem.py
@@ -0,0 +1,2 @@
+my_dict = {'a': 1, 'b': 2}
+item = my_dict.popitem()  # item could be ('a', 1) or ('b', 2)
---
+++ values.py
@@ -0,0 +1,2 @@
+my_dict = {'a': 1, 'b': 2}
+values = my_dict.values()  # Returns dict_values([1, 2])
